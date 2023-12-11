81156f19fb8538effe6d3cf7ecf9fd097a88af2f
Ritesh Khatri <riteshkhatri1512@gmail.com>
2023-12-11 07:38:03 +0530

    Refactored and Updated ReplyMessage

---
+++ src/main/java/com/beta/replyservice/model/ReplyMessage.java
@@ -0,0 +1,17 @@
+package com.beta.replyservice.model;
+
+import lombok.Data;
+
+@Data
+public class ReplyMessage {
+
+	private final String data;
+
+	// public ReplyMessage(String data) {
+	// this.data = data;
+	// }
+
+	// public String getData() {
+	// return data;
+	// }
+}
